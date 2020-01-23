cb91b0848d7db799590ccf6a1a365e5c4d29c2b2
Andrey Radionov <a.radionov@animarender.com>
2020-01-23 11:19:07 +0500

    Added Exceptions

---
+++ src/Exceptions/PathNotExistsException.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace FileManager\Exceptions;
+
+use Throwable;
+
+/**
+ * Class PathNotExistsException
+ * @package FileManager\Exceptions
+ */
+class PathNotExistsException extends \Exception
+{
+    /**
+     * InvalidPathException constructor.
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, 0, $previous);
+    }
+}

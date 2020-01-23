25c7cdf66a3ad9552d5aec0023b119564fcf9cc6
Andrey Radionov <a.radionov@animarender.com>
2020-01-23 11:06:38 +0500

    Added realPath to PathUtils trait

--- src/Traits/PathUtils.php
+++ src/Traits/PathUtils.php
@@ -17,4 +17,16 @@ trait PathUtils {
         return trim($path, '/');
     }
 
+    /**
+     * @param $path
+     * @return false|\RuntimeException|string
+     */
+    public function realPath($path)
+    {
+        $path = realpath($path);
+        if ($path === false) {
+            return new \RuntimeException('Path not exists');
+        }
+        return $path;
+    }
 }
